a4d8106fb22f844966bf59c4722d97c1b6d88e98
Tay Ray Chuan <rctay89@gmail.com>
2012-02-26 21:32:14 +0800

    justify components

--- lib/commentgrades/render/default.rb
+++ lib/commentgrades/render/default.rb
@@ -4,16 +4,34 @@ module CommentGrades
   module Render
     class Default < Mustache
       self.template = \
-"""report for {{student}}:
+"""report for {{student}}:{{prep_components}}
 {{#components}}
 {{name}}: {{grade}}/{{outof}}
 {{/components}}
 {{#final_adjustment}}
 final {{adjustment}}
 {{/final_adjustment}}
-final: {{grade}}/{{outof}}
+{{final_justified}}: {{grade}}/{{outof}}
 
 """
+
+      @width = 0
+      def prep_components
+        c = self[:components]
+        @width = c.map {|d|
+          d[:name] = d[:name].to_s
+          d[:name].size
+        }.max + 1
+        c.each do |d|
+          d[:name] = d[:name].ljust(@width)
+        end
+        # don't render anything when called
+        ''
+      end
+
+      def final_justified
+        "final".ljust @width
+      end
     end
   end
 end
